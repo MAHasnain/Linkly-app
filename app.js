5c1352bb9f7bab2a5ad6f821c153b0eb851c608d
Muhammad Atta Ul Hasnain <134517609+MAHasnain@users.noreply.github.com>
2025-09-29 21:39:13 +0500

    feat(supabase): setup database service and render contacts on homepage
    Configured Supabase client with project URL and anon key
    Implemented CRUD operations for Contacts (fetch, getById, create, update, delete)
    Added authentication methods (register, login, session retrieval, get user)
    Centralized DB and auth logic in a single service file for reusability
    Integrated Supabase fetch function on index.html
    Successfully rendered all contacts dynamically on homepage

---
+++ app.js
@@ -0,0 +1,50 @@
+import { fetchContact, retrieveSession } from "./database.js";
+
+// insertData({ first_name: 'MA', last_name: "Hasnain", phone_number: 3121234567 , email: "email@email.com" })
+
+// updateContact(1, { first_name: "Muhammad", last_name: 'Atta' });
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const contacts_sec = document.querySelector(".all-contacts");
+
+    try {
+        const session = await retrieveSession()
+        console.log(session);
+
+        const contacts = await fetchContact();
+        console.log(contacts)
+
+        contacts_sec.innerHTML = "";
+        contacts.map(contact => {
+            contacts_sec.innerHTML += `
+             <div class="contact" data-id="${contact.id}">
+                    <div class="contact-info">
+                        <div class="user-icon"><i class="fa-solid fa-circle-user"></i></div>
+                        <div class="name-number">
+                            <p class="name">${contact.first_name} ${contact.last_name}</p>
+                            <p class="number">${contact.phone_number}</p>
+                        </div>
+                    </div>
+                    <div class="contact-icons">
+                        <span><i class="fa-solid fa-phone"></i></span>
+                        <span><i class="fa-solid fa-message"></i></span>
+                        <span><i class="fa-solid fa-envelope"></i></span>
+                    </div>
+                </div>`
+        })
+
+        const allContacts = document.querySelectorAll(".contact");
+        allContacts.forEach(contact => {
+            console.log(contact)
+            contact.addEventListener("click", () => {
+                console.log("clicked")
+                const contactId = contact.getAttribute("data-id");
+                window.location.href = `/HTML/contactDetail.html?id=${contactId}`;
+            })
+        })
+
+    } catch (error) {
+        console.error(error);
+    }
+})
+
