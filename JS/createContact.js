f2018a7c8bc0e060c270d152ec5810d8c5457cbb
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2025-10-04 17:19:33 +0500

    feat: add contact creation form with logic - Built create contact form (first name, last name, phone, email) - Added validation for required fields - Integrated Supabase insert function (createContact) - Success alert + form reset on contact creation - Error handling for failed creation

--- JS/createContact.js
+++ JS/createContact.js
@@ -4,17 +4,35 @@ const firstNameInp = document.querySelector("#first_name_inp")
 const lastNameInp = document.querySelector("#last_name_inp")
 const phoneNumberInp = document.querySelector("#phone_number_inp")
 const emailInp = document.querySelector("#email_inp")
-const createBtn = document.querySelector("#createBtn")
+const createContactForm = document.querySelector("#createContactForm")
+const errorMsg = document.querySelector(".error-msg");
 
-createBtn.addEventListener("click", async (e) => {
+createContactForm.addEventListener("submit", async (e) => {
     e.preventDefault();
     try {
 
-        const created = await createContact({ first_name: firstNameInp.value, last_name: lastNameInp.value, phone_number: phoneNumberInp.value, email: emailInp.value, })
+        const created = await createContact({ first_name: firstNameInp.value, last_name: lastNameInp.value, phone_number: phoneNumberInp.value, email: emailInp.value });
 
-        console.log(created)
+        errorMsg.textContent = ``;
+
+        if (!firstNameInp.value || !lastNameInp.value || !phoneNumberInp.value || !emailInp.value) {
+            errorMsg.textContent = `Please fill out all required fields!`;
+            return;
+        }
+
+        console.log("created", created);
+        if (created.data == null) {
+
+        }
+
+        if (created.data) {
+            swal("Done!", "Contact created successfully!", "success");
+            createContactForm.reset();
+        }
 
     } catch (error) {
         console.error(error)
+        errorMsg.innerHTML = `Something went wrong!.`
+
     }
 })
