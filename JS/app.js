b1ec8834db80c1501207da62ab0ef8b615ace090
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2025-10-06 22:46:04 +0500

    feat: created unauthenticated user page with login/register buttons and added styled logout button

--- JS/app.js
+++ JS/app.js
@@ -1,8 +1,9 @@
-import { fetchContact, retrieveSession } from "./database.js";
+import { fetchContact, logoutUser, retrieveSession } from "./database.js";
 
 // insertData({ first_name: 'MA', last_name: "Hasnain", phone_number: 3121234567 , email: "email@email.com" })
 
 // updateContact(1, { first_name: "Muhammad", last_name: 'Atta' });
+const logoutBtn = document.querySelector(".logout-btn");
 
 document.addEventListener("DOMContentLoaded", async () => {
     const contacts_sec = document.querySelector(".all-contacts");
@@ -11,40 +12,49 @@ document.addEventListener("DOMContentLoaded", async () => {
         const session = await retrieveSession()
         console.log(session);
 
-        const contacts = await fetchContact();
-        console.log(contacts)
+        if (session) {
 
-        contacts_sec.innerHTML = "";
-        contacts.map(contact => {
-            contacts_sec.innerHTML += `
-             <div class="contact" data-id="${contact.id}">
-                    <div class="contact-info">
-                        <div class="user-icon"><i class="fa-solid fa-circle-user"></i></div>
-                        <div class="name-number">
-                            <p class="name">${contact.first_name} ${contact.last_name}</p>
-                            <p class="number">${contact.phone_number}</p>
-                        </div>
-                    </div>
-                    <div class="contact-icons">
-                        <span><a href="tel:${contact.phone_number}"><i class="fa-solid fa-phone"></i></a></span>
-                        <span><a href=""><i class="fa-solid fa-message"></i></a></span>
-                        <span><a href="mailto:${contact.email}"><i class="fa-solid fa-envelope"></i></a></span>
-                    </div>
-                </div>`
-        })
+            const contacts = await fetchContact();
+            console.log(contacts)
 
-        const allContacts = document.querySelectorAll(".contact");
-        allContacts.forEach(contact => {
-            console.log(contact)
-            contact.addEventListener("click", () => {
-                console.log("clicked")
-                const contactId = contact.getAttribute("data-id");
-                window.location.href = `/HTML/contactDetail.html?id=${contactId}`;
+            contacts_sec.innerHTML = "";
+            contacts.map(contact => {
+                contacts_sec.innerHTML += `
+               <div class="contact" data-id="${contact.id}">
+               <div class="contact-info">
+               <div class="user-icon"><i class="fa-solid fa-circle-user"></i></div>
+               <div class="name-number">
+               <p class="name">${contact.first_name} ${contact.last_name}</p>
+               <p class="number">${contact.phone_number}</p>
+               </div>
+               </div>
+               <div class="contact-icons">
+               <span><a href="tel:${contact.phone_number}"><i class="fa-solid fa-phone"></i></a></span>
+               <span><a href=""><i class="fa-solid fa-message"></i></a></span>
+               <span><a href="mailto:${contact.email}"><i class="fa-solid fa-envelope"></i></a></span>
+               </div>
+               </div>`
             })
-        })
-
+            const allContacts = document.querySelectorAll(".contact");
+            allContacts.forEach(contact => {
+                console.log(contact)
+                contact.addEventListener("click", () => {
+                    console.log("clicked")
+                    const contactId = contact.getAttribute("data-id");
+                    window.location.href = `/HTML/contactDetail.html?id=${contactId}`;
+                })
+            })
+        }
     } catch (error) {
         console.error(error);
     }
 })
 
+
+logoutBtn.addEventListener("click", async (e) => {
+    e.preventDefault();
+    // console.log("Clicked");
+
+    const loggedOut = await logoutUser()
+
+})
